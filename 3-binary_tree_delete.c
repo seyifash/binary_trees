80935bae2b4fe81a01cbf46d3a0ed9ad5fa37fc2
seyifash <fasugbeoluwaseyi@gmail.com>
2023-08-29 00:05:27 +0100

    checking root nodes

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_delete -a function that deletes an entire binary tree
+ * @node: the node to be deleted 
+*/
+
 void binary_tree_delete(binary_tree_t *tree)
 {
     if (tree == NULL)
@@ -8,6 +13,6 @@ void binary_tree_delete(binary_tree_t *tree)
 
     binary_tree_delete(tree->left);
     binary_tree_delete(tree->right);
-    
+
     free(tree);
 }
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,4 +1,11 @@
 #include "binary_trees.h"
+#include <stdio.h>
+
+/**
+ * binary_tree_is_leaf - function that checks if a node is a leaf
+ * @node: the node to be checked
+ * Return: returns 1 if its a leaf node, else returns 0
+*/
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
