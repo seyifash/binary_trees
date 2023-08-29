a23a9a30deecc9ea5d90fb6c502ced194a984025
seyifash <fasugbeoluwaseyi@gmail.com>
2023-08-29 01:13:04 +0100

    postprder and inorder traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - function that goes through a binary tree using pre-order traversal
+ * @tree: the tree to be printed 
+ * @func: the function pointer used in printing the tree
+ * 
+*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (tree == NULL || func == NULL)
+    {
+        return;
+    }
+    func(tree->n);
+    binary_tree_preorder(tree->left, func);
+    binary_tree_preorder(tree->right, func);
+}
---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - function that goes through a binary tree using pre-order traversal
+ * @tree: the tree to be printed 
+ * @func: the function pointer used in printing the tree
+ * 
+*/
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (tree == NULL || func == NULL)
+    {
+        return;
+    }
+    binary_tree_inorder(tree->left, func);
+    func(tree->n);
+    binary_tree_inorder(tree->right, func);
+}
