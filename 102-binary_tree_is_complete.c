5e3867f361ef151ec11016b5555ae11fccd8d688
osei <williamsabodunrin@gmail.com>
2023-08-31 04:47:43 -0700

    check if tree is complete

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -24,74 +24,53 @@ queue_node_t *create_queue_node(const binary_tree_t *node)
 }
 
 /**
- * create_queue - the creation of a new node
- * Return: the new node
+ * free_queue - frees queue
+ * @head: pointer to head
  */
 
-queue_t *create_queue()
+void free_queue(queue_node_t *head)
 {
-	queue_t *queue;
+	queue_node_t *tmp;
 
-	queue = malloc(sizeof(queue_t));
-	if (queue == NULL)
+	while (head != NULL)
 	{
-	return (NULL);
+		tmp = head->next;
+		free(head);
+		head = tmp;
 	}
-	queue->front = queue->rear = NULL;
-	return (queue);
 }
 
 /**
- * enqueue - enqueues the node in the queue
- * @queue: the pointer to the queue
+ * push_into - pushes node to back of queue
+ * @head: the pointer to the head
+ * @tail: the pointer to tail
  * @node: the node
  */
-void enqueue(queue_t *queue, const binary_tree_t *node)
+void push_into(queue_node_t *head, queue_node_t **tail, binary_tree_t *node)
 {
 	queue_node_t *new_node;
 
 	new_node = create_queue_node(node);
 	if (new_node == NULL)
 	{
-	return;
+		free_queue(head);
+		exit(1);
 	}
-
-	if (queue->rear == NULL)
-	{
-	queue->front = queue->rear = new_node;
-	return;
-	}
-
-	queue->rear->next = new_node;
-	queue->rear = new_node;
+	(*tail)->next = new_node;
+	*tail = new_node;
 }
 
 /**
- * dequeue - Function to dequeue a node from the queue
- * @queue: the queue
- * Return: the dequeue node
+ * pop_head - removes head
+ * @head: the pointer to head of queue
  */
-const binary_tree_t *dequeue(queue_t *queue)
+void pop_head(queue_node_t **head)
 {
-	const binary_tree_t *node;
 	queue_node_t *temp;
 
-	if (queue->front == NULL)
-	{
-	return (NULL);
-	}
-
-	temp = queue->front;
-	node = temp->node;
-	queue->front = queue->front->next;
-
-	if (queue->front == NULL)
-	{
-	queue->rear = NULL;
-	}
-
-	free(temp);
-	return (node);
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
 }
 /**
  * binary_tree_is_complete - Function to check if a binary tree is complete
@@ -101,39 +80,43 @@ const binary_tree_t *dequeue(queue_t *queue)
 
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	queue_t *queue;
-	int reached_null;
-	const binary_tree_t *current;
+	queue_node_t *head, *tail;
+	int checker = 0;
 
 	if (tree == NULL)
-	{
-	return (0);
-	}
+		return (0);
 
-	queue = create_queue();
-	reached_null = 0;
+	head = tail = create_queue_node((binary_tree_t *)tree);
+	if (head == NULL)
+		exit(1);
 
-	enqueue(queue, tree);
+	while (head != NULL)
+	{
+		if (head->node->left != NULL)
+		{
+			if (checker == 1)
+			{
+				free_queue(head);
+				return (0);
+			}
+			push_into(head, &tail, head->node->left);
+		}
+		else
+			checker = 1;
 
-	while (queue->front != NULL)
-	{
-	current = dequeue(queue);
-	if (current == NULL)
-	{
-	reached_null = 1;
-	}
-	else
-	{
-	if (reached_null)
-	{
-	free(queue);
-	return (0);
-	}
+		if (head->node->right != NULL)
+		{
+			if (checker == 1)
+			{
+				free_queue(head);
+				return (0);
+			}
+			push_into(head, &tail, head->node->right);
+		}
+		else
+			checker = 1;
 
-	enqueue(queue, current->left);
-	enqueue(queue, current->right);
+		pop_head(&head);
 	}
-	}
-	free(queue);
 	return (1);
 }
