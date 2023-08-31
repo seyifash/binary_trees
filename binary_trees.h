a7a6a70ad44401f02eb6fa587c1a04ecc3692418
osei <williamsabodunrin@gmail.com>
2023-08-30 23:36:05 -0700

    adding to header file

--- binary_trees.h
+++ binary_trees.h
@@ -86,7 +86,11 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *secoond);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *find_min(bst_t *node);
 bst_t *bst_remove(bst_t *root, int value);
+bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_insert(bst_t **tree, int value);
+int binary_tree_is_bst(const binary_tree_t *tree);
 #endif
